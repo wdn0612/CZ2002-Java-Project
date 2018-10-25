020b142e8304ee0a6068853321e2e00d5bde86bf
Yuehann <zyh990424@gmail.com>
2018-10-25 16:54:17 +0800

    Update src/courseStructure.java

--- src/courseStructure.java
+++ src/courseStructure.java
@@ -12,4 +12,45 @@ public class courseStructure {
 	public courseStructure() {
 		
 	}
+	// Create a new courseStructure given weight of finalExam, quiz, assignment, project, participation
+	public courseStructure(double finalExam, double quiz, double assignment, double project, double participation) {
+		this.finalExam=finalExam;
+		this.quiz=quiz;
+		this.assignment=assignment;
+		this.project=project;
+		this.participation=participation;
+	}
+/*---------------------------------------------------------------- */
+	// Get the weight of each assessment
+	public double getWeightOfFinal() {
+		return this.finalExam;
+	}
+	public double getWeightOfQuiz() {
+		return this.quiz;
+	}
+	public double getWeightOfAssignment() {
+		return this.assignment;
+	}
+	public double getWeightOfProject() {
+		return this.project;
+	}
+	public double getWeightOfParticipation() {
+		return this.participation;
+	}
+	// Set the weight of each assessment
+	public void setWeightOfFinal(double finalExam) {
+		this.finalExam=finalExam;
+	}
+	public void setWeightOfQuiz(double quiz) {
+		this.quiz=quiz;
+	}
+	public void setWeightOfAssignment(double assignment) {
+		this.assignment=assignment;
+	}
+	public void setWeightOfProject(double project) {
+		this.project=project;
+	}
+	public void setWeightOfParticipation(double participation) {
+		this.participation=participation;
+	}
 }
